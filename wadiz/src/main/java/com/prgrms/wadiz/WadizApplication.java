40776dbd5b86d7657de8b6ab2500f6ca110e05eb
HandmadeCloud <joje1003@gmail.com>
2023-08-23 11:44:12 +0900

    project init

---
+++ wadiz/src/main/java/com/prgrms/wadiz/WadizApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.wadiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WadizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WadizApplication.class, args);
+	}
+
+}
