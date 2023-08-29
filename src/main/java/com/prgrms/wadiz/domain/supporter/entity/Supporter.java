9887ebf76bfa8cd65ebde872d07bceaeef9165aa
born-A <ra_ari@naver.com>
2023-08-29 21:46:56 +0900

    feat: Supporter toDTO 메서드 추가

---
+++ src/main/java/com/prgrms/wadiz/domain/supporter/entity/Supporter.java
@@ -0,0 +1,49 @@
+package com.prgrms.wadiz.domain.supporter.entity;
+
+import com.prgrms.wadiz.domain.supporter.dto.request.SupporterCreateRequestDTO;
+import com.prgrms.wadiz.domain.supporter.dto.response.SupporterCreateResponseDTO;
+import com.prgrms.wadiz.global.BaseEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "supporters")
+@NoArgsConstructor
+public class Supporter extends BaseEntity {
+    @Id
+    @Column(name="supporter_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+
+    @Builder
+    public Supporter(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void changeName(String name) {
+        this.name = name;
+    }
+
+    public void changeEmail(String email) {
+        this.email = email;
+    }
+
+    public static SupporterCreateRequestDTO toDTOForRequest(Supporter supporter) {
+        return new SupporterCreateRequestDTO(supporter.getName(), supporter.getEmail());
+    }
+
+    public static SupporterCreateResponseDTO toDTOForResponse(Supporter supporter) {
+        return new SupporterCreateResponseDTO(supporter.getId(),supporter.getName(), supporter.getEmail());
+    }
+
+}
