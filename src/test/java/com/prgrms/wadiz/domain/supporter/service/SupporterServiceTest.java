d72dbf32f2b2b75bf61d63c88f8b60d01d91c774
born-A <ra_ari@naver.com>
2023-08-30 18:34:33 +0900

    test : supporter update test

--- src/test/java/com/prgrms/wadiz/domain/supporter/service/SupporterServiceTest.java
+++ src/test/java/com/prgrms/wadiz/domain/supporter/service/SupporterServiceTest.java
@@ -1,7 +1,8 @@
 package com.prgrms.wadiz.domain.supporter.service;
 
 import com.prgrms.wadiz.domain.supporter.dto.request.SupporterCreateRequestDTO;
-import com.prgrms.wadiz.domain.supporter.dto.response.SupporterCreateResponseDTO;
+import com.prgrms.wadiz.domain.supporter.dto.request.SupporterUpdateRequestDTO;
+import com.prgrms.wadiz.domain.supporter.dto.response.SupporterResponseDTO;
 import com.prgrms.wadiz.domain.supporter.entity.Supporter;
 import com.prgrms.wadiz.domain.supporter.repository.SupporterRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,21 +27,41 @@ class SupporterServiceTest {
 
     @BeforeEach
     void setUp() {
-        supporter = new Supporter("testName","test");
+        supporter = new Supporter("testName", "test");
     }
+
     @Test
     @DisplayName("서포터를 생성하여 저장한다")
-    void saveTest(){
+    void saveTest() {
         //given
         SupporterCreateRequestDTO supporterDTO = new SupporterCreateRequestDTO(supporter.getName(),
                 supporter.getEmail());
 
         //when
-        SupporterCreateResponseDTO supporterResponse = supporterService.createSupporter(supporterDTO);
+        SupporterResponseDTO supporterResponse = supporterService.createSupporter(supporterDTO);
 
         //then
         assertThat(supporterResponse).isNotNull();
     }
 
+    @Test
+    @DisplayName("서포터의 정보를 수정한다.")
+    void updateTest() {
+        //given
+        SupporterCreateRequestDTO insertSupporter= new SupporterCreateRequestDTO(supporter.getName(),
+                supporter.getEmail());
 
+        SupporterResponseDTO responseDTO = supporterService.createSupporter(insertSupporter);
+
+        SupporterUpdateRequestDTO supporterUpdateRequestDTO = new SupporterUpdateRequestDTO("update",
+                "update@gmail.com");
+
+        //when
+        SupporterResponseDTO supporterResponseDTO = supporterService.updateSupporter(responseDTO.toEntity().getId(),
+                supporterUpdateRequestDTO);
+
+        //then
+        assertThat(supporterResponseDTO.toEntity().getId()).isEqualTo(responseDTO.toEntity().getId());
+        assertThat(supporterResponseDTO.name()).isEqualTo("update");
+    }
 }
