0a1df4ff2281c82bca7c9865e37cfc90e134ce34
hi-june <wnsgur735@gmail.com>
2023-09-02 00:16:14 +0900

    :recycle: refactor: 불필요한 어노테이션 삭제

--- wadiz/src/main/java/com/prgrms/wadiz/domain/funding/repository/FundingRepository.java
+++ wadiz/src/main/java/com/prgrms/wadiz/domain/funding/repository/FundingRepository.java
@@ -2,8 +2,6 @@ package com.prgrms.wadiz.domain.funding.repository;
 
 import com.prgrms.wadiz.domain.funding.entity.Funding;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface FundingRepository extends JpaRepository<Funding, Long> {
 }
