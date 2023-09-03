595c8f40730d32f58e1efd02af525ed363996f5d
born-A <ra_ari@naver.com>
2023-09-03 11:25:28 +0900

    refactor : service 반환 형식 변경

--- wadiz/src/main/java/com/prgrms/wadiz/domain/supporter/service/SupporterService.java
+++ wadiz/src/main/java/com/prgrms/wadiz/domain/supporter/service/SupporterService.java
@@ -1,7 +1,6 @@
 package com.prgrms.wadiz.domain.supporter.service;
 
-import com.prgrms.wadiz.domain.supporter.dto.request.SupporterCreateRequestDTO;
-import com.prgrms.wadiz.domain.supporter.dto.request.SupporterUpdateRequestDTO;
+import com.prgrms.wadiz.domain.supporter.dto.request.SupporterRequestDTO;
 import com.prgrms.wadiz.domain.supporter.dto.response.SupporterResponseDTO;
 import com.prgrms.wadiz.domain.supporter.entity.Supporter;
 import com.prgrms.wadiz.domain.supporter.repository.SupporterRepository;
@@ -19,24 +18,32 @@ public class SupporterService {
     }
 
     @Transactional
+    public SupporterResponseDTO signUpSupporter(SupporterRequestDTO dto) {
+        Supporter supporter = Supporter.builder()
+                .name(dto.name())
+                .email(dto.email())
+                .build();
 
-    public SupporterResponseDTO createSupporter(SupporterCreateRequestDTO dto) {
-        Supporter entity = dto.toEntity();
-        Supporter savedEntity = supporterRepository.save(entity);
-        return Supporter.toDTOForResponse(savedEntity);
+        Supporter savedEntity = supporterRepository.save(supporter);
+        return SupporterResponseDTO.of(savedEntity.getSupporterName(),savedEntity.getSupporterEmail());
     }
 
     public SupporterResponseDTO getSupporter(Long id) {
-        Supporter supporter =supporterRepository.findById(id).orElseThrow(() -> new RuntimeException("수정할 id에 해당하는 서포터가 없습니다"));
-        return Supporter.toDTOForResponse(supporter);
+        Supporter supporter =supporterRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("수정할 id에 해당하는 서포터가 없습니다"));
+
+        return SupporterResponseDTO.of(supporter.getSupporterName(),supporter.getSupporterEmail());
     }
 
-    public SupporterResponseDTO updateSupporter(Long id, SupporterUpdateRequestDTO dto) {
-        Supporter supporter =supporterRepository.findById(id).orElseThrow(() -> new RuntimeException("수정할 id에 해당하는 서포터가 없습니다"));
+    public SupporterResponseDTO updateSupporter(Long id, SupporterRequestDTO dto) {
+        Supporter supporter =supporterRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("수정할 id에 해당하는 서포터가 없습니다"));
+
         supporter.changeName(dto.name());
         supporter.changeEmail(dto.email());
+
         Supporter savedSupporter = supporterRepository.save(supporter);
-        return Supporter.toDTOForResponse(savedSupporter);
+        return SupporterResponseDTO.of(savedSupporter.getSupporterName(),savedSupporter.getSupporterEmail());
     }
 
 
