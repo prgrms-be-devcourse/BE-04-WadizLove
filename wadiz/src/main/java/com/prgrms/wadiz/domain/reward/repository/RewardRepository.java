11f001324f6d02e9a8f146205d3a87d4982bb591
born-A <ra_ari@naver.com>
2023-09-04 17:20:38 +0900

    refactor : RewardService에서 ProjectRepo 참조하지 않도록 수정

--- wadiz/src/main/java/com/prgrms/wadiz/domain/reward/repository/RewardRepository.java
+++ wadiz/src/main/java/com/prgrms/wadiz/domain/reward/repository/RewardRepository.java
@@ -4,6 +4,13 @@ import com.prgrms.wadiz.domain.reward.entity.Reward;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RewardRepository extends JpaRepository<Reward,Long> {
+
+    public Optional<List<Reward>> findAllByProjectId(Long projectId);
+
+    Reward findByProjectId(Long projectId);
 }
