cb4675309fd298ad7ba3b4bef6afb9453640f60e
born-A <ra_ari@naver.com>
2023-09-04 23:17:38 +0900

    style

--- wadiz/src/main/java/com/prgrms/wadiz/domain/reward/repository/RewardRepository.java
+++ wadiz/src/main/java/com/prgrms/wadiz/domain/reward/repository/RewardRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface RewardRepository extends JpaRepository<Reward,Long> {
 
     public Optional<List<Reward>> findAllByProjectId(Long projectId);
