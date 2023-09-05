be160e448808048d436b8635eb444d6afa34a98e
hi-june <wnsgur735@gmail.com>
2023-09-05 10:29:19 +0900

    feat: funding service layer에 CRUD 요청 전파를 위한 project service 정의

--- wadiz/src/main/java/com/prgrms/wadiz/domain/project/service/ProjectService.java
+++ wadiz/src/main/java/com/prgrms/wadiz/domain/project/service/ProjectService.java
@@ -1,11 +1,14 @@
 package com.prgrms.wadiz.domain.project.service;
 
+import com.prgrms.wadiz.domain.funding.dto.request.FundingCreateRequestDTO;
+import com.prgrms.wadiz.domain.funding.dto.request.FundingUpdateRequestDTO;
 import com.prgrms.wadiz.domain.funding.dto.response.FundingResponseDTO;
 import com.prgrms.wadiz.domain.funding.service.FundingServiceFacade;
 import com.prgrms.wadiz.domain.maker.dto.response.MakerResponseDTO;
 import com.prgrms.wadiz.domain.maker.entity.Maker;
 import com.prgrms.wadiz.domain.maker.service.MakerService;
 import com.prgrms.wadiz.domain.post.dto.response.PostResponseDTO;
+import com.prgrms.wadiz.domain.project.dto.ProjectServiceDTO;
 import com.prgrms.wadiz.domain.reward.dto.response.RewardResponseDTO;
 import com.prgrms.wadiz.global.util.exception.ErrorCode;
 import com.prgrms.wadiz.domain.post.service.PostServiceFacade;
@@ -77,5 +80,30 @@ public class ProjectService {
         return ProjectResponseDTO.of(projectId, makerResponseDTO, postServiceDTO, fundingServiceDTO, rewardServiceDTOs);
     }
 
-    //하위 도메인 생성 : 하위 도메인 서비스에게 생성을 위임한다.
+    @Transactional
+    public void createFunding(Long projectId, FundingCreateRequestDTO fundingCreateRequestDTO) {
+        Project project = projectRepository.findById(projectId)
+                .orElseThrow(() -> new BaseException(ErrorCode.PROJECT_NOT_FOUND));
+        ProjectServiceDTO projectServiceDTO = ProjectServiceDTO.from(project);
+
+        fundingServiceFacade.createFunding(projectServiceDTO, fundingCreateRequestDTO);
+    }
+
+    @Transactional(readOnly = true)
+    public FundingResponseDTO getFunding(Long projectId) {
+        return fundingServiceFacade.getFundingByProjectId(projectId);
+    }
+
+    @Transactional
+    public void updateFunding(
+            Long projectId,
+            FundingUpdateRequestDTO fundingUpdateRequestDTO
+    ) {
+        fundingServiceFacade.updateFunding(projectId, fundingUpdateRequestDTO);
+    }
+
+    @Transactional
+    public void deleteFunding(Long projectId) {
+        fundingServiceFacade.deleteFunding(projectId);
+    }
 }
