52cd9dc6765f506c83f64ec4cb672fa09aaec1c8
hi-june <wnsgur735@gmail.com>
2023-09-05 12:13:44 +0900

    feat: projectId 필드로 post를 조회하는 쿼리 추가

--- wadiz/src/main/java/com/prgrms/wadiz/domain/post/repository/PostRepository.java
+++ wadiz/src/main/java/com/prgrms/wadiz/domain/post/repository/PostRepository.java
@@ -2,6 +2,15 @@ package com.prgrms.wadiz.domain.post.repository;
 
 import com.prgrms.wadiz.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    @Query("SELECT p FROM Post p WHERE p.project.projectId = :projectId")
+    Optional<Post> findByProjectId(@Param("projectId") Long projectId);
+
+    @Query("DELETE FROM Post p WHERE p.project.projectId = :projectId")
+    void deleteByProjectId(@Param("projectId") Long projectId);
 }
