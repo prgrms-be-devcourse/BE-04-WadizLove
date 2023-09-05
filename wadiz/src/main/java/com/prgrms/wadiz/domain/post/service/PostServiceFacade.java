3997c82019a30a9e5b29444b8754ceb18c62814b
hi-june <wnsgur735@gmail.com>
2023-09-05 12:14:15 +0900

    feat: post CRUD service 구현

--- wadiz/src/main/java/com/prgrms/wadiz/domain/post/service/PostServiceFacade.java
+++ wadiz/src/main/java/com/prgrms/wadiz/domain/post/service/PostServiceFacade.java
@@ -1,9 +1,12 @@
 package com.prgrms.wadiz.domain.post.service;
 
 import com.prgrms.wadiz.domain.post.dto.request.PostCreateRequestDTO;
+import com.prgrms.wadiz.domain.post.dto.request.PostUpdateRequestDTO;
 import com.prgrms.wadiz.domain.post.dto.response.PostResponseDTO;
 import com.prgrms.wadiz.domain.post.entity.Post;
 import com.prgrms.wadiz.domain.post.repository.PostRepository;
+import com.prgrms.wadiz.domain.project.dto.ProjectServiceDTO;
+import com.prgrms.wadiz.domain.project.entity.Project;
 import com.prgrms.wadiz.global.util.exception.BaseException;
 import com.prgrms.wadiz.global.util.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -16,30 +19,54 @@ public class PostServiceFacade {
     private final PostRepository postRepository;
 
     @Transactional
-    public Post createPost(PostCreateRequestDTO postCreateRequestDTO) {
+    public Long createPost(
+            ProjectServiceDTO projectServiceDTO,
+            PostCreateRequestDTO postCreateRequestDTO
+    ) {
+        Project project = ProjectServiceDTO.toEntity(projectServiceDTO);
+
         Post post = Post.builder()
+                .project(project)
                 .postTitle(postCreateRequestDTO.postTitle())
                 .postDescription(postCreateRequestDTO.postDescription())
                 .postThumbNailImage(postCreateRequestDTO.postThumbNailImage())
                 .postContentImage(postCreateRequestDTO.postContentImage())
                 .build();
 
-        return postRepository.save(post);
+        return postRepository.save(post).getPostId();
+    }
+
+    @Transactional(readOnly = true)
+    public PostResponseDTO getPostByProjectId(Long projectId) {
+        Post post = postRepository.findByProjectId(projectId)
+                .orElseThrow(() -> new BaseException(ErrorCode.POST_NOT_FOUND));
+
+        return PostResponseDTO.from(post);
     }
 
     @Transactional
-    public PostResponseDTO getPost(Long postId) {
-        Post post = postRepository.findById(postId)
+    public void updatePost(
+            Long projectId,
+            PostUpdateRequestDTO postUpdateRequestDTO
+    ) {
+        Post post = postRepository.findByProjectId(projectId)
                 .orElseThrow(() -> new BaseException(ErrorCode.POST_NOT_FOUND));
 
-        return PostResponseDTO.toResponseDTO(post);
+        post.updatePost(
+                postUpdateRequestDTO.postTitle(),
+                postUpdateRequestDTO.postDescription(),
+                postUpdateRequestDTO.postThumbNailImage(),
+                postUpdateRequestDTO.postContentImage()
+        );
     }
 
+    @Transactional(readOnly = true)
     public boolean isPostExist(Long projectId) {
-        return false;
+        return postRepository.findByProjectId(projectId).isPresent();
     }
 
-    public PostResponseDTO getPostByProjectId(Long projectId) {
-        return null;
+    @Transactional
+    public void deletePost(Long projectId) {
+        postRepository.deleteByProjectId(projectId);
     }
 }
