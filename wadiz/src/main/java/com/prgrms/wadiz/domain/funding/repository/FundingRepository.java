ea03c55b051b88a492ab5213d1f538c4c67d47f4
hi-june <wnsgur735@gmail.com>
2023-09-05 10:32:52 +0900

    feat: projectId 필드로 funding을 조회하는 쿼리 추가

--- wadiz/src/main/java/com/prgrms/wadiz/domain/funding/repository/FundingRepository.java
+++ wadiz/src/main/java/com/prgrms/wadiz/domain/funding/repository/FundingRepository.java
@@ -2,6 +2,15 @@ package com.prgrms.wadiz.domain.funding.repository;
 
 import com.prgrms.wadiz.domain.funding.entity.Funding;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface FundingRepository extends JpaRepository<Funding, Long> {
+    @Query("SELECT f FROM Funding f WHERE f.project.projectId = :projectId")
+    Optional<Funding> findByProjectId(@Param("projectId") Long projectId);
+
+    @Query("DELETE FROM Funding f WHERE f.project.projectId = :projectId")
+    void deleteByProjectId(@Param("projectId") Long projectId);
 }
