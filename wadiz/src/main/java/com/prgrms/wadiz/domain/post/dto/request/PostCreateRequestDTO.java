01c1b41c86150c9fb414df25d4fee5a79ea57e59
hi-june <wnsgur735@gmail.com>
2023-09-06 19:04:55 +0900

    feat: request DTO에 필드 검증 추가

--- wadiz/src/main/java/com/prgrms/wadiz/domain/post/dto/request/PostCreateRequestDTO.java
+++ wadiz/src/main/java/com/prgrms/wadiz/domain/post/dto/request/PostCreateRequestDTO.java
@@ -2,11 +2,21 @@ package com.prgrms.wadiz.domain.post.dto.request;
 
 import lombok.Builder;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Builder
 public record PostCreateRequestDTO(
+        @NotBlank(message = "제목은 비워둘 수 없습니다.")
         String postTitle,
+
+        @NotBlank(message = "상세 설명은 비워둘 수 없습니다.")
         String postDescription,
+
+        @Pattern(regexp = "^https?://.*\\.(?:png|jpg|jpeg|gif)$", message = "올바른 이미지 URL 형식이 아닙니다.")
         String postThumbNailImage,
+
+        @Pattern(regexp = "^https?://.*\\.(?:png|jpg|jpeg|gif)$", message = "올바른 이미지 URL 형식이 아닙니다.")
         String postContentImage
 ) {
 }
