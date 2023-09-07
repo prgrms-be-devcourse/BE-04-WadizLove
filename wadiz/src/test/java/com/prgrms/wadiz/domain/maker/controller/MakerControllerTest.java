6a714d780f2579876191dbd2ffb219d1790231b9
born-A <ra_ari@naver.com>
2023-09-07 20:54:27 +0900

    test : maker, supporter dto validation

--- wadiz/src/test/java/com/prgrms/wadiz/domain/maker/controller/MakerControllerTest.java
+++ wadiz/src/test/java/com/prgrms/wadiz/domain/maker/controller/MakerControllerTest.java
@@ -3,8 +3,10 @@ package com.prgrms.wadiz.domain.maker.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prgrms.wadiz.domain.maker.dto.request.MakerCreateRequestDTO;
 import com.prgrms.wadiz.domain.maker.dto.request.MakerUpdateRequestDTO;
+import com.prgrms.wadiz.domain.maker.dto.response.MakerResponseDTO;
 import com.prgrms.wadiz.domain.maker.entity.Maker;
 import com.prgrms.wadiz.domain.maker.service.MakerService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,11 +14,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -80,6 +91,7 @@ class MakerControllerTest {
                 .build();
 
         String json = mapper.writeValueAsString(updateRequestDTO);
+
         //when & then
         mvc.perform(put("/makers/" + 1L)
                         .content(json)
@@ -101,9 +113,87 @@ class MakerControllerTest {
                 .andExpect(jsonPath(code, 0).exists())
                 .andExpect(jsonPath(message, "성공했습니다.").exists());
     }
+
+    @Test
+    @DisplayName("[실패] 잘못된 데이터로 인한 메이커 생성 실패 테스트")
+    void invalidNameTest() throws Exception {
+        //given
+        MakerCreateRequestDTO dto = MakerCreateRequestDTO.builder()
+                .makerName(" ")
+                .makerBrand(maker.getMakerBrand())
+                .makerEmail(maker.getMakerEmail())
+                .build();
+
+        String json = mapper.writeValueAsString(dto);
+
+        //조두
+        MockHttpServletRequestBuilder builder = post("/makers/sign-up")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mvc.perform(builder).andReturn();
+
+        String message = Objects.requireNonNull(result.getResolvedException()).getMessage();
+        System.out.println("message = " + message);
+
+        Assertions.assertThat(HttpStatus.BAD_REQUEST);
+        Assertions.assertThat(message).contains("이름은 최소 2자 이상입니다.");
+    }
+
+    @Test
+    @DisplayName("[실패] 잘못된 데이터로 인한 메이커 생성 실패 테스트")
+    void invalidBrandTest() throws Exception {
+        //given
+        MakerCreateRequestDTO dto = MakerCreateRequestDTO.builder()
+                .makerName(maker.getMakerName())
+                .makerBrand(" ")
+                .makerEmail(maker.getMakerEmail())
+                .build();
+
+        String json = mapper.writeValueAsString(dto);
+
+        //조두
+        MockHttpServletRequestBuilder builder = post("/makers/sign-up")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mvc.perform(builder).andReturn();
+
+        String message = Objects.requireNonNull(result.getResolvedException()).getMessage();
+        System.out.println("message = " + message);
+
+        Assertions.assertThat(HttpStatus.BAD_REQUEST);
+        Assertions.assertThat(message).contains("브랜드를 입력해주세요.");
+    }
+
+    @Test
+    @DisplayName("[실패] 잘못된 데이터로 인한 메이커 생성 실패 테스트")
+    void invalidEmailTest() throws Exception {
+        //given
+        MakerCreateRequestDTO dto = MakerCreateRequestDTO.builder()
+                .makerName(maker.getMakerName())
+                .makerBrand(maker.getMakerBrand())
+                .makerEmail("aaa")
+                .build();
+
+        String json = mapper.writeValueAsString(dto);
+
+        //when & then
+        MockHttpServletRequestBuilder builder = post("/makers/sign-up")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mvc.perform(builder).andReturn();
+
+        String message = Objects.requireNonNull(result.getResolvedException()).getMessage();
+        System.out.println("message = " + message);
+
+        Assertions.assertThat(HttpStatus.BAD_REQUEST);
+        Assertions.assertThat(message).contains("이메일 형식이 맞지 않습니다.");
+    }
+
+
 }
-
-
 
 
 
--- wadiz/src/test/java/com/prgrms/wadiz/domain/supporter/controller/SupporterControllerTest.java
+++ wadiz/src/test/java/com/prgrms/wadiz/domain/supporter/controller/SupporterControllerTest.java
@@ -1,18 +1,25 @@
 package com.prgrms.wadiz.domain.supporter.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.prgrms.wadiz.domain.maker.dto.request.MakerCreateRequestDTO;
 import com.prgrms.wadiz.domain.supporter.dto.request.SupporterCreateRequestDTO;
 import com.prgrms.wadiz.domain.supporter.dto.request.SupporterUpdateRequestDTO;
 import com.prgrms.wadiz.domain.supporter.entity.Supporter;
 import com.prgrms.wadiz.domain.supporter.service.SupporterService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.util.Objects;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -95,4 +102,55 @@ class SupporterControllerTest {
                 .andExpect(jsonPath(code, 0).exists())
                 .andExpect(jsonPath(message, "성공했습니다.").exists());
     }
+
+    @Test
+    @DisplayName("[성공] 잘못된 데이터로 인한 서포터 생성 실패 테스트")
+    void invalidEmailTest() throws Exception {
+        //given
+        SupporterCreateRequestDTO dto = SupporterCreateRequestDTO.builder()
+                .supporterName(supporter.getSupporterName())
+                .supporterEmail("aaa")
+                .build();
+
+        String json = mapper.writeValueAsString(dto);
+
+        //when & then
+        MockHttpServletRequestBuilder builder = post("/supporters/sign-up")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mvc.perform(builder).andReturn();
+
+        String message = Objects.requireNonNull(result.getResolvedException()).getMessage();
+        System.out.println("message = " + message);
+
+        Assertions.assertThat(HttpStatus.BAD_REQUEST);
+        Assertions.assertThat(message).contains("이름은 최소 2자 이상입니다.","이메일 형식이 맞지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("[성공] 잘못된 데이터로 인한 서포터 생성 실패 테스트")
+    void invalidNameTest() throws Exception {
+        //given
+        SupporterCreateRequestDTO dto = SupporterCreateRequestDTO.builder()
+                .supporterName(" ")
+                .supporterEmail(supporter.getSupporterEmail())
+                .build();
+
+        String json = mapper.writeValueAsString(dto);
+
+        //when & then
+        MockHttpServletRequestBuilder builder = post("/supporters/sign-up")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mvc.perform(builder).andReturn();
+
+        String message = Objects.requireNonNull(result.getResolvedException()).getMessage();
+        System.out.println("message = " + message);
+
+        Assertions.assertThat(HttpStatus.BAD_REQUEST);
+        Assertions.assertThat(message).contains("이름은 최소 2자 이상입니다.");
+    }
+
 }
