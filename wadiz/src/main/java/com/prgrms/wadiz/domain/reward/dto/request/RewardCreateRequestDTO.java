e92e37a13390ff8197371a3ffe6ce2e63dbc8bdd
born-A <ra_ari@naver.com>
2023-09-13 16:08:34 +0900

    feat : 리워드 swagger api 어노테이션

--- wadiz/src/main/java/com/prgrms/wadiz/domain/reward/dto/request/RewardCreateRequestDTO.java
+++ wadiz/src/main/java/com/prgrms/wadiz/domain/reward/dto/request/RewardCreateRequestDTO.java
@@ -3,6 +3,7 @@ package com.prgrms.wadiz.domain.reward.dto.request;
 import com.prgrms.wadiz.domain.reward.RewardStatus.RewardStatus;
 import com.prgrms.wadiz.domain.reward.RewardType.RewardType;
 import com.prgrms.wadiz.global.annotation.ValidEnum;
+import io.swagger.annotations.ApiParam;
 import lombok.Builder;
 
 import javax.validation.constraints.Min;
@@ -10,21 +11,27 @@ import javax.validation.constraints.NotBlank;
 
 @Builder
 public record RewardCreateRequestDTO(
+        @ApiParam(value = "리워드명")
         @NotBlank(message = "리워드명을 입력해주세요.")
         String rewardName,
 
+        @ApiParam(value = "리워드 설명")
         @NotBlank(message = "리워드 설명을 입력해주세요.")
         String rewardDescription,
 
+        @ApiParam(value = "리워드 재고")
         @Min(value = 1, message = "리워드 재고는 최소 1개 이상입니다.")
         Integer rewardQuantity,
 
+        @ApiParam(value = "리워드 가격")
         @Min(value = 10, message = "리워드 가격을 입력해주세요.")
         Integer rewardPrice,
 
+        @ApiParam(value = "리워드 타입")
         @ValidEnum(enumClass = RewardType.class, message = "리워드 타입을 입력해주세요.")
         RewardType rewardType,
 
+        @ApiParam(value = "리워드 상태")
         @ValidEnum(enumClass = RewardStatus.class, message = "리워드 타입을 입력해주세요.")
         RewardStatus rewardStatus
 ) {
--- wadiz/src/main/java/com/prgrms/wadiz/domain/reward/dto/response/RewardResponseDTO.java
+++ wadiz/src/main/java/com/prgrms/wadiz/domain/reward/dto/response/RewardResponseDTO.java
@@ -3,16 +3,24 @@ package com.prgrms.wadiz.domain.reward.dto.response;
 import com.prgrms.wadiz.domain.reward.RewardStatus.RewardStatus;
 import com.prgrms.wadiz.domain.reward.RewardType.RewardType;
 import com.prgrms.wadiz.domain.reward.entity.Reward;
+import io.swagger.annotations.ApiParam;
 import lombok.Builder;
 
 @Builder
 public record RewardResponseDTO(
+        @ApiParam(value = "리워드 아이디")
         Long rewardId,
+        @ApiParam(value = "리워드명")
         String rewardName,
+        @ApiParam(value = "리워드 설명")
         String rewardDescription,
+        @ApiParam(value = "리워드 재고")
         Integer rewardQuantity,
+        @ApiParam(value = "리워드 가격")
         Integer rewardPrice,
+        @ApiParam(value = "리워드 타입")
         RewardType rewardType,
+        @ApiParam(value = "리워드 상태")
         RewardStatus rewardStatus
 ) {
     public static RewardResponseDTO from(Reward reward){
