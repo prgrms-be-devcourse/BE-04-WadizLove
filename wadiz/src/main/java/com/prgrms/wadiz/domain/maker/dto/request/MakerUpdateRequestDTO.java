075b42e54ce3ef7f1c62da5676b381630486a017
born-A <ra_ari@naver.com>
2023-09-19 16:44:22 +0900

    feat : swagger maker api response, parameter 구체화

--- wadiz/src/main/java/com/prgrms/wadiz/domain/maker/dto/request/MakerUpdateRequestDTO.java
+++ wadiz/src/main/java/com/prgrms/wadiz/domain/maker/dto/request/MakerUpdateRequestDTO.java
@@ -9,15 +9,15 @@ import javax.validation.constraints.Size;
 
 @Builder
 public record MakerUpdateRequestDTO(
-        @Schema(description = "메이커 이름")
+        @Schema(description = "메이커 이름", example = "alohaJune")
         @Size(min = 2, message = "이름은 최소 2자 이상입니다.")
         String makerName,
 
-        @Schema(description = "메이커 브랜드")
+        @Schema(description = "메이커 브랜드", example = "Lotte")
         @NotBlank(message = "브랜드를 입력해주세요.")
         String makerBrand,
 
-        @Schema(description = "메이커 이메일")
+        @Schema(description = "메이커 이메일", example = "lotte@gmail.com")
         @Email(message = "이메일 형식이 맞지 않습니다.")
         String makerEmail
 ) {
--- wadiz/src/main/java/com/prgrms/wadiz/domain/maker/entity/Maker.java
+++ wadiz/src/main/java/com/prgrms/wadiz/domain/maker/entity/Maker.java
@@ -40,17 +40,6 @@ public class Maker extends BaseEntity{
     @ValidEnum(enumClass = MakerStatus.class, message = "해당하는 메이커 상태가 존재하지 않습니다.")
     private MakerStatus status = MakerStatus.REGISTERED;
 
-//    @Builder // 빌더가 이렇게도 생성될 수 있는지 알아보기 + 추가 설정으로
-//    public Maker(
-//            String makerName,
-//            String makerBrand,
-//            String makerEmail
-//    ) {
-//        this.makerName = makerName;
-//        this.makerBrand = makerBrand;
-//        this.makerEmail = makerEmail;
-//    }
-
     @Builder
     public Maker(
             Long makerId,
@@ -64,8 +53,6 @@ public class Maker extends BaseEntity{
         this.makerEmail = makerEmail;
     }
 
-    //TODO : 중복 검사 메소드 넣기
-
     public void updateMaker(
             String makerName,
             String makerEmail,
@@ -73,19 +60,6 @@ public class Maker extends BaseEntity{
     ){
         this.makerName = makerName;
         this.makerBrand = makerBrand;
-        this.makerEmail = makerEmail;
-    }
-
-
-    public void changeMakerName(String makerName) {
-        this.makerName = makerName;
-    }
-
-    public void chaneMakerBrand(String makerBrand) {
-        this.makerBrand = makerBrand;
-    }
-
-    public void changeMakerEmail(String makerEmail) {
         this.makerEmail = makerEmail;
     }
 
